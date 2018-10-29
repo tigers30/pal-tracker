c44dd58548a1d6207a44c91dd40a86a9e9f6c208
traine <trainee@ford.com>
2018-10-29 16:11:42 -0400

    added contollers and manifest

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,39 @@
+//package io.pivotal.pal.tracker;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class WelcomeController {
+//
+//    @Value("${WELCOME_MESSAGE}")
+//    String message;
+//
+//    @GetMapping("/")
+//    public String sayHello() {
+//        return "hello";
+//    }
+//}
+
+
 package io.pivotal.pal.tracker;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public final String message;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message){
+        this.message = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
